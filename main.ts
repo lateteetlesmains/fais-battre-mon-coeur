8f8c50551cd1512c3f0a8107fa1282a10fd4bad3
lateteetlesmains <127963157+lateteetlesmains@users.noreply.github.com>
2023-03-15 15:37:02 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.showIcon(IconNames.Heart)
-//  basic.pause(100)
-basic.forever(function on_forever() {
+// basic.pause(100)
+basic.forever(function () {
     basic.showIcon(IconNames.SmallHeart)
-    //  basic.pause(100)
+    // basic.pause(100)
     basic.showIcon(IconNames.Heart)
 })
