a441451e219564813dc9ef1129386a28af6268ea
lateteetlesmains <127963157+lateteetlesmains@users.noreply.github.com>
2023-03-15 15:34:25 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.showIcon(IconNames.Heart)
+//  basic.pause(100)
+basic.forever(function on_forever() {
+    basic.showIcon(IconNames.SmallHeart)
+    //  basic.pause(100)
+    basic.showIcon(IconNames.Heart)
+})
